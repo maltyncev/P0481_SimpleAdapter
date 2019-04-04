76f1992a0f9fa3e6ed35062765b09fd9591c32dc
maltyncev <maltyncev@gmail.com>
2019-04-04 12:51:30 +0500

    Initial commit

---
+++ app/src/main/java/ru/startandroid/p0481_simpleadapter/item.java
@@ -0,0 +1,13 @@
+package ru.startandroid.p0481_simpleadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
